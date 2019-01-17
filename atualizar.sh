e7abf872a86f3a7e79c45dddf1236cec7a19dc53
Nick21Araujo <46789040+Nick21Araujo@users.noreply.github.com>
2019-01-17 16:16:13 -0200

    Create atualizar.sh

---
+++ atualizar.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Script para atualizar o GNU/Linux
+
+echo Atualizando as listas do Apt
+echo 
+	sudo apt update
+echo
+echo Listas Atualizadas 
+
+echo Atualizando os Software 
+echo 
+	sudo apt -y upgrade
+echo
+echo Software Atualizados
+
+echo Atualizando o Kernel
+echo 
+	sudo apt dist-upgrade
+echo
+echo Kernel Atualizado
+
+echo Removendo Software
+echo 
+	sudo apt autoremove
+echo
+echo Software Removedo 
+
+echo Limpado o Cache
+echo 
+	sudo apt -y autoclean
+echo
+echo Cache Limpo
