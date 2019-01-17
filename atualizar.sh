16a8bfcc9b77c445badb8b178eda274d4f038fb6
Nick21Araujo <46789040+Nick21Araujo@users.noreply.github.com>
2019-01-17 16:34:16 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -6,27 +6,37 @@ echo
 	sudo apt update
 echo
 echo Listas Atualizadas 
+sleep 5
+clear
 
 echo Atualizando os Software 
 echo 
 	sudo apt -y upgrade
 echo
 echo Software Atualizados
+sleep 5
+clear
 
 echo Atualizando o Kernel
 echo 
 	sudo apt dist-upgrade
 echo
 echo Kernel Atualizado
+sleep 5
+clear
 
 echo Removendo Software
 echo 
 	sudo apt autoremove
 echo
 echo Software Removido 
+sleep 5
+clear
 
 echo Limpado o Cache
 echo 
 	sudo apt -y autoclean
 echo
 echo Cache Limpo
+sleep 5
+clear
